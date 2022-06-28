d99fc2450ebd481733134e9d780713d05352fd4b
Gabriel Burgos <gabrielburgosmm@gmail.com>
2022-06-28 14:13:11 -0700

    commit tree c181dd47f1e893e98f4af1386f494cd28b2459a1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+// function returnFirstTwoDrivers() {
+   //  const newDrivers = drivers.splice(2,2);
+   // return newDrivers;
+//}
+
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+  };
+
+
+function returnLastTwoDrivers (drivers) {
+    return drivers.slice(-2);
+  };
+
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  function createfFreMultiplier(multiplier) {
+    let fare;
+    return fare * multiplier;
+  }
+
+
+
